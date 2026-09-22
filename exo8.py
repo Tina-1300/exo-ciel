7292681bffd153def6fe7f355839d13e2d167705
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2026-09-22 17:01:04 +0200

    fix: factorial

--- exo8.py
+++ exo8.py
@@ -1,4 +1,7 @@
-import math
 
-resultat = math.factorial(5)  
-print(resultat)
+user = int(input("Entrez le nombre : "))
+
+factorial = lambda n: n * factorial(n-1) if n != 0 else 1
+
+print(f"factorielle de {user} est {factorial(user)}")  
+
