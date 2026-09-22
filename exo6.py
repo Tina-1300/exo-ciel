df212cfd16b9f745f2ae0f39b521159ba46a2704
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2026-09-22 17:06:11 +0200

    fix: exo6

--- exo6.py
+++ exo6.py
@@ -1,3 +1,5 @@
-value1 = 3
-value2 = 2
+nb1 = float(input("Entrez vôtre premier nombre : "))
+nb2 = float(input("Entrez vôtre 2eme nombre : "))
+value1 = nb1
+value2 = nb2
 print(f"Le minimum est {min(value1, value2)} et le maximum {max(value1, value2)}")
