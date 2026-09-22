14d0cdfec47f2431cd59a387779d2c7ca813ece0
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2026-09-22 17:15:35 +0200

    fix: exo7

--- exo7.py
+++ exo7.py
@@ -1,8 +1,11 @@
+user_price_ht = float(input("Entrez le prix ht : "))
+user_tva = float(input("Entrez la tva : "))
+
 def calculer_montant_ttc(prix_ht, tva) :
   montant_ttc = prix_ht * (1 + (tva / 100))
   return round(montant_ttc, 2)
 
-prix_ht = 20
-tva = 20
+prix_ht = user_price_ht #20
+tva = user_tva #20
 
 print(f"Le montant TTC est : {calculer_montant_ttc(prix_ht, tva)}")
