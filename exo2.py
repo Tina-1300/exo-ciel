0fc4e1f332ba17d17777758ac839ba77dabe4659
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2026-09-22 17:11:18 +0200

    fix: exo2

--- exo2.py
+++ exo2.py
@@ -1,2 +1,5 @@
-def calcule_addition(prix, pourboire=15):return float(prix) * (pourboire / 100) + float(prix)
-print(f"Le montant total de vôtre addition est {calcule_addition(15.0, 10)}")
+user_price = float(input("Entrez le prix : "))
+user_pourboire = float(input("Entrez la valeur du pourboire : "))
+
+def calcule_addition(prix, pourboire):return float(prix) * (pourboire / 100) + float(prix)
+print(f"Le montant total de vôtre addition est {calcule_addition(user_price, user_pourboire)}")
